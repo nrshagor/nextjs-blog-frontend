61a00501b0c723886bd885682250754c5dc6fa90
nrshagor <noorerabbishagor@gmail.com>
2024-08-30 01:50:04 +0600

    next ui setup and navbar create

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,34 +1,133 @@
 "use client";
-import Link from "next/link";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+} from "@nextui-org/react";
 import React, { useEffect } from "react";
 import "../style/navbar.scss";
 import LogoutButton from "./LogoutButton";
 import { usePathname } from "next/navigation";
 import { getCookie } from "cookie-handler-pro";
 
-const Navbar = () => {
+const Navbars = () => {
   const token = getCookie("token");
   const path = usePathname();
+
+  const menuItems = [
+    { name: "Blog", path: "/blog/view" },
+    { name: "All Blog", path: "/blog" },
+    { name: "Dashboard", path: "/dashboard" },
+  ];
+
   useEffect(() => {
     console.log(path);
   }, [path]);
+
   return (
-    <div className="navbar">
-      <Link href="/">Home</Link>
-      {!token ? (
-        <>
-          <Link href="/login">Login</Link>
-          <Link href="/register">register</Link>
-        </>
-      ) : (
-        <>
-          <Link href="/dashboard">Dashboard</Link>
-          {/* <Link href="/dashboard/profile">Profile</Link> */}
-          <LogoutButton />
-        </>
-      )}
-    </div>
+    <>
+      <Navbar disableAnimation isBordered>
+        <NavbarContent className="sm:hidden" justify="start">
+          <NavbarMenuToggle />
+        </NavbarContent>
+
+        <NavbarContent className="sm:hidden pr-3" justify="center">
+          <NavbarBrand>
+            <Link color="foreground" href="/">
+              <p className="font-bold text-inherit">Blog Pro</p>
+            </Link>
+          </NavbarBrand>
+        </NavbarContent>
+
+        <NavbarContent className="hidden sm:flex gap-4" justify="center">
+          <NavbarBrand>
+            <Link color="foreground" href="/">
+              <p className="font-bold text-inherit">Blog Pro</p>
+            </Link>
+          </NavbarBrand>
+          <NavbarItem>
+            <Link color="foreground" href="/blog/view">
+              Blog
+            </Link>
+          </NavbarItem>
+          <NavbarItem isActive={path === "/blog"}>
+            <Link href="/blog" aria-current="page" color="warning">
+              All Blog
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+
+        <NavbarContent justify="end">
+          {!token ? (
+            <>
+              <NavbarItem>
+                <Link href="/login">Login</Link>
+              </NavbarItem>
+              <NavbarItem>
+                <Button
+                  as={Link}
+                  color="warning"
+                  href="/register"
+                  variant="flat"
+                >
+                  Sign Up
+                </Button>
+              </NavbarItem>
+            </>
+          ) : (
+            <>
+              <NavbarItem>
+                <Link href="/dashboard">Dashboard</Link>
+              </NavbarItem>
+              <LogoutButton />
+            </>
+          )}
+        </NavbarContent>
+
+        <NavbarMenu>
+          {menuItems.map((item, index) => (
+            <NavbarMenuItem key={`${item.name}-${index}`}>
+              <Link
+                className="w-full"
+                color={item.name === "Dashboard" ? "warning" : "foreground"}
+                href={item.path}
+                size="lg"
+              >
+                {item.name}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+          {!token && (
+            <>
+              <NavbarMenuItem>
+                <Link className="w-full" href="/login" size="lg">
+                  Login
+                </Link>
+              </NavbarMenuItem>
+              <NavbarMenuItem>
+                <Link className="w-full" href="/register" size="lg">
+                  Sign Up
+                </Link>
+              </NavbarMenuItem>
+            </>
+          )}
+          {token && (
+            <NavbarMenuItem>
+              <Link className="w-full" href="#" size="lg">
+                <LogoutButton />
+              </Link>
+            </NavbarMenuItem>
+          )}
+        </NavbarMenu>
+      </Navbar>
+    </>
   );
 };
 
-export default Navbar;
+export default Navbars;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import BlogPost from "./components/BlogPost";
 import BlogPostView from "./components/BlogPostView";
+import { Button } from "@nextui-org/react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>hello next</h1>
+      <Button>Click me</Button>
       <BlogPost />
 
       <BlogPostView />
